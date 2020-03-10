7486be6612d977b46b7a450fb3857def05d2c71b
RYOTA IIJIMA <jimar884@gmail.com>
2020-03-10 23:35:53 +0900

    コンパイル実行し、exeファイルを生成

--- ProjectAOJ/ProjectAOJ/Form1.cs
+++ ProjectAOJ/ProjectAOJ/Form1.cs
@@ -24,6 +24,13 @@ namespace ProjectAOJ
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //コンパイラ起動
+            //二つ目の引数はコンパイルしたいcppファイルの保存場所およびファイル名
+            //a.exeは\AOJ\ProjectAOJ\ProjectAOJ\bin\Debugに保存される模様
+            System.Diagnostics.Process.Start("g++", @"C:\Users\Owner\source\repos\AOJ\ProjectAOJ\ProjectAOJ\bin\Debug\test.cpp");
+            //生成された実行ファイルを実行
+            //二つ目の引数は実行したいexeファイルの保存場所およびファイル名
+            //System.Diagnostics.Process.Start("a.exe", @"C:\Users\Owner\source\repos\AOJ\ProjectAOJ\ProjectAOJ\bin\Debug\a.exe");
 
         }
     }
